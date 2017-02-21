bfb403f4632a2c7659897aa36ff8dea3264d4989
Stefan Azarić <azaricstefan@yahoo.com>
2017-02-21 13:54:31 +0100

    Bugfix sada radi i na Note4 (mobilni)

--- app/src/main/java/com/azaric/tiltmaze/GameActivity.java
+++ app/src/main/java/com/azaric/tiltmaze/GameActivity.java
@@ -11,7 +11,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.azaric.tiltmaze.DB.DbOperationsHelper;
@@ -48,19 +47,19 @@ public class GameActivity extends Activity
 
         //create model and controller
         controller = new Controller(this);
-        model = new Polygon(this,getApplicationContext());
+        model = new Polygon(this, getApplicationContext());
         controller.setModel(model);
 
         //create imageView and connect it with model and controller
         MyImageView imageView = (MyImageView) findViewById(R.id.my_image_view);
-        imageView.setController(this,controller);
+        imageView.setController(this, controller);
         controller.setImageView(imageView);
         imageView.setModel(model);
 
         dbOperationsHelper = new DbOperationsHelper(this);
 
         //SENSOR CODE
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 
@@ -69,7 +68,7 @@ public class GameActivity extends Activity
         Intent intent = getIntent();
         controller.nameOfPolygonToLoad = intent.getStringExtra(MainActivity.NAME_OF_POLYGON);
         if (controller.nameOfPolygonToLoad != null) {
-            model.loadPolygonFromFile(controller.nameOfPolygonToLoad,this);
+            model.loadPolygonFromFile(controller.nameOfPolygonToLoad, this);
             long num = model.getGameTime();
             setGameTime(num);
         } else
@@ -81,32 +80,32 @@ public class GameActivity extends Activity
     public void onBackPressed() {
         super.onBackPressed();
         sensorManager.unregisterListener(this);
-        if(!model.isGameOver()) {
-            getController().getModel().setGameTime((System.currentTimeMillis() - startTime) / 1000);
+        if (!model.isGameOver()) {
+            getController().getModel().setGameTime((long) gameTime + (System.currentTimeMillis() - startTime) / 1000);
             getController().getModel().savePolygon("TEMP:" + filterTmp(controller.getNameOfPolygonToLoad()) + ":tmp", this);
         }
     }
 
-    public void recreate(){
+    public void recreate() {
         setContentView(R.layout.activity_game);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         startTime = System.currentTimeMillis();
 
         //create model and controller
         controller = new Controller(this);
-        model = new Polygon(this,getApplicationContext());
+        model = new Polygon(this, getApplicationContext());
         controller.setModel(model);
 
         //create imageView and connect it with model and controller
         MyImageView imageView = (MyImageView) findViewById(R.id.my_image_view);
-        imageView.setController(this,controller);
+        imageView.setController(this, controller);
         controller.setImageView(imageView);
         imageView.setModel(model);
 
         dbOperationsHelper = new DbOperationsHelper(this);
 
         //SENSOR CODE
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         long num = model.getGameTime();
@@ -116,10 +115,10 @@ public class GameActivity extends Activity
     @Override
     protected void onResume() {
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-        if(!firstLaunch) {
+        if (!firstLaunch) {
             File[] files = getApplicationContext().getExternalFilesDir(null).listFiles();
-            for(int i = 0; i < files.length; i++){
-                if(files[i].getName().equals("TEMP:" + controller.getNameOfPolygonToLoad() + ":tmp")) {
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].getName().equals("TEMP:" + controller.getNameOfPolygonToLoad() + ":tmp")) {
                     //onCreate(null);
                     recreate();
                     getController().getModel().loadPolygonFromFile(files[i].getName(), this);
@@ -142,24 +141,24 @@ public class GameActivity extends Activity
         super.onPause();
         firstLaunch = false;
         sensorManager.unregisterListener(this);
-        if(!model.isGameOver()) {
-            getController().getModel().setGameTime((System.currentTimeMillis() - startTime) / 1000);
+        if (!model.isGameOver()) {
+            getController().getModel().setGameTime((long) gameTime + (System.currentTimeMillis() - startTime) / 1000);
             getController().getModel().savePolygon("TEMP:" + filterTmp(controller.getNameOfPolygonToLoad()) + ":tmp", this);
 
         }
     }
 
-    public String filterTmp(String name){
+    public String filterTmp(String name) {
         String[] full = name.split(":");
-        if(full.length > 1)
+        if (full.length > 1)
             return full[1];
         return name;
     }
 
-    public void deleteTmpPolygon(){
+    public void deleteTmpPolygon() {
         File[] files = getApplicationContext().getExternalFilesDir(null).listFiles();
-        for(int i = 0; i < files.length; i++){
-            if(files[i].getName().equals("TEMP:" + filterTmp(controller.getNameOfPolygonToLoad()) + ":tmp")) {
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].getName().equals("TEMP:" + filterTmp(controller.getNameOfPolygonToLoad()) + ":tmp")) {
                 files[i].delete();
             }
         }
@@ -180,70 +179,81 @@ public class GameActivity extends Activity
         controller.addNewAccelerometerValues(x, y, time);
         getController().getModel().setGameTime((System.currentTimeMillis() - startTime) / 1000);
         boolean gameOver = model.isGameOver();
-        if(gameOver && firstTime){
-            if(model.isWin()) {
+        if (gameOver && firstTime) {
+            if (model.isWin()) {
                 firstTime = false;
                 firstLaunch = false;
                 endTime = System.currentTimeMillis();
                 score = getGameTime();
                 Toast t;
-                t = Toast.makeText(this, R.string.game_won,Toast.LENGTH_SHORT);
+                t = Toast.makeText(this, R.string.game_won, Toast.LENGTH_SHORT);
                 t.show();
                 //otvori dialog
                 DialogFragment nameDialog = new GameNameDialog();
                 nameDialog.show(getFragmentManager(), "nameDialog");
             } else {
-                firstLaunch = false;
-                Toast t;
-                t = Toast.makeText(getApplicationContext(), R.string.game_lost,Toast.LENGTH_SHORT);
-                t.show();
-//                new AsyncTask<Void,Void,Void>(){
-//
-//                    @Override
-//                    protected Void doInBackground(Void... params) {
-//                        deleteTmpPolygon();
-//                        Log.d("PUCA", "OVDE");
-//                        return null;
-//                    }
-//                }.execute();
-                deleteTmpPolygon();
-//                if(controller.nameOfPolygonToLoad.contains("TEMP")) {
-//                    File[] files = getApplicationContext().getExternalFilesDir(null).listFiles();
-//                    for(int i = 0; i < files.length; i++){
-//                        if(files[i].getName().equals(controller.nameOfPolygonToLoad))
-//                            files[i].delete();
-//                    }
-//                }
-                finish();
+                if (firstLaunch) {
+                    firstLaunch = false;
+                    Toast t;
+                    t = Toast.makeText(getApplicationContext(), R.string.game_lost, Toast.LENGTH_SHORT);
+                    t.show();
+                    new AsyncTask<Void, Void, Void>() {
+
+                        @Override
+                        protected Void doInBackground(Void... params) {
+                            deleteTmpPolygon();
+                            Log.d("PUCA", "OVDE");
+                            return null;
+                        }
+                    }.execute();
+                    //deleteTmpPolygon();
+                    finish();
+                }
             }
         }
 
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
 
-    public Controller getController() { return controller; }
+    public Controller getController() {
+        return controller;
+    }
 
-    public DbOperationsHelper getDbOperationsHelper() { return dbOperationsHelper; }
+    public DbOperationsHelper getDbOperationsHelper() {
+        return dbOperationsHelper;
+    }
 
-    public void setStartTime(long startTime) { this.startTime = startTime; }
-    public void setEndTime(long endTime) { this.endTime = endTime; }
-    public float getScore() { return score; }
-    public float getGameTime() { return gameTime + (((float)(endTime - startTime)) / 1000f); }
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public float getGameTime() {
+        return gameTime + (((float) (endTime - startTime)) / 1000f);
+    }
 
     @Override
     public void play(int i) {
-        Log.d("muzika", "lallala "+i);
-        services=new Intent(this, MyService.class);
+        Log.d("muzika", "lallala " + i);
+        services = new Intent(this, MyService.class);
         services.putExtra("naziv", i);
         services.setAction(MyService.ACTION_PLAY);
         startService(services);
     }
 
     public double getCurrentGameTime() {
-        return gameTime + (((float)(System.currentTimeMillis() - startTime)) / 1000f);
+        return gameTime + (((float) (System.currentTimeMillis() - startTime)) / 1000f);
     }
 
     public void setGameTime(float gameTime) {
